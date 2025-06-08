e8b36a57851863a305c9583d0270536d6076cf2f
Liudmila <bauklu@mail.ru>
2025-06-08 10:28:32 +0300

    google router commented

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
 
 from app.api.endpoints import (
-    google_api_router, charity_project_router, donation_router, user_router)
+    #google_api_router,
+    charity_project_router,
+    donation_router, user_router
+)
 
 main_router = APIRouter()
 main_router.include_router(
